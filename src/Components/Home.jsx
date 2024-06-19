08ea7b687ec0b82737d7be305a4537697020873b
Harris G <harrisgus1@hotmail.com>
2024-06-19 00:33:26 +0000

    Changed Home background image

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,26 +1,9 @@
-/**
- * Home component
- *
- * The section at the top of the page to display image of your
- * choice, name and title that describes your career focus.
- */
-
 import React from "react";
 import arrowSvg from "../images/down-arrow.svg";
 import PropTypes from "prop-types";
 
-/**
- * Home background image
- *
- * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
- * represents what you see in that image.
- *
- *
- * Need an image? Check out https://unsplash.com to download a photo you
- * freely use on your site.
- */
-import image from "../images/thinking_man.jpg";
+
+import image from "../images/WhiteCityScape.jpg";
 
 const imageAltText = "Thinking Man";
 
@@ -33,7 +16,11 @@ const Home = ({ name, title }) => {
         <h2>{title}</h2>
       </div>
       <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
-        <img src={arrowSvg} style={{ height: "3rem", width: "3rem", animation: "bounce 2s infinite" }} alt={imageAltText} />
+        <img
+          src={arrowSvg}
+          style={{ height: "3rem", width: "3rem", animation: "bounce 2s infinite" }}
+          alt={imageAltText}
+        />
       </div>
     </section>
   );
