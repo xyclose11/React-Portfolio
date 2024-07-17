0e454c79c7ddd886898929e73834dca73108ee2e
Harris G <harrisgus1@hotmail.com>
2024-07-17 20:40:28 +0000

    added gradient backgrounds to container comp.

--- src/Components/Components.jsx
+++ src/Components/Components.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Container = ({ size, children }) => {
+const Container = ({ size, children, backgroundColor, backgroundImage }) => {
   const containerStyle = {
     borderRadius: "10px",
     padding: "10px",
-    backgroundColor: "#2b3773",
+    backgroundColor: backgroundColor || "#2b3773",
     justifyContent: "center",
+    backgroundImage: backgroundImage || "none",
     alignItems: "center",
+    "text-align": "center",
     width: size === "small" ? "200px" : size === "medium" ? "325px" : "600px",
 };
 
@@ -17,6 +19,8 @@ const Container = ({ size, children }) => {
 Container.propTypes = {
   size: PropTypes.oneOf(["small", "medium", "large"]),
   children: PropTypes.node,
+  backgroundColor: PropTypes.string,
+  backgroundImage: PropTypes.string,
 };
 
 export default Container;
--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -11,39 +11,51 @@ const Education = () => {
       <h2 style={{ textAlign: "center", color: "#333", marginBottom: "20px" }}>Education</h2>
     
         <div className="education" style={{ display: "flex", justifyContent: "space-evenly" }}>
-          <Container size={"small"}>
-            <h3 style={{ color: "white" }}>University of Illinois Urbana-Champaign</h3>
-            <p style={{ color: "white" }}>B.S. Computer Science</p>
-            <p style={{ color: "white" }}>Graduated: May 2021</p>
+          <Container size={"medium"} backgroundImage={"linear-gradient(317deg, rgba(167,78,94,1) 5%, rgba(106,100,177,1) 100%)"}>
+            <h3 style={{ color: "white" }}>Ivy Tech Community College</h3>
+            <h3 style={{ color: "white" }}>A.A.S. Cyber Security/Information Assurance</h3>
+            <p style={{ color: "white" }}>Graduated: December 2022</p>
           </Container>
-          <Container size={"small"}>
-            <h3 style={{ color: "white" }}>University of Illinois Urbana-Champaign</h3>
-            <p style={{ color: "white" }}>B.S. Computer Science</p>
-            <p style={{ color: "white" }}>Graduated: May 2021</p>
+          <Container size={"medium"} backgroundImage={"linear-gradient(317deg, rgba(167,78,94,1) 5%, rgba(106,100,177,1) 100%)"}>
+            <h3 style={{ color: "white" }}>Valparaiso University</h3>
+            <h3 style={{ color: "white" }}>B.S. Computer Science</h3>
+            <p style={{ color: "white" }}>Graduating: May 2025</p>
           </Container>
-          <Container size={"small"}>
-            <h3 style={{ color: "white" }}>University of Illinois Urbana-Champaign</h3>
-            <p style={{ color: "white" }}>B.S. Computer Science</p>
-            <p style={{ color: "white" }}>Graduated: May 2021</p>
+          <Container size={"medium"} backgroundImage={"linear-gradient(317deg, rgba(167,78,94,1) 5%, rgba(106,100,177,1) 100%)"}>
+            <h3 style={{ color: "white" }}>Valparaiso University</h3>
+            <h3 style={{ color: "white" }}>M.S. Cyber Security</h3>
+            <p style={{ color: "white" }}>Graduating: May 2027</p>
           </Container>
         </div>
 
       <div style={{marginTop: "30px"}}>
 
-        <h2 style={{ textAlign: "center", color: "#333", marginBottom: "20px" }}>External Courses</h2>
+        <h2 style={{ textAlign: "center", color: "#333", marginBottom: "20px" }}>
+          External Courses
+        </h2>
 
         <div className="education" style={{ display: "flex", justifyContent: "space-evenly" }}>
-          <Container size={"small"}>
+          <Container size={"small"} backgroundImage={"linear-gradient(to right, #642b73, #c6426e)"}>
             <h3 style={{ color: "white" }}>University of Illinois Urbana-Champaign</h3>
             <p style={{ color: "white" }}>B.S. Computer Science</p>
             <p style={{ color: "white" }}>Graduated: May 2021</p>
           </Container>
-          <Container size={"small"}>
+          <Container size={"small"} backgroundImage={"linear-gradient(to right, #642b73, #c6426e)"}>
             <h3 style={{ color: "white" }}>University of Illinois Urbana-Champaign</h3>
             <p style={{ color: "white" }}>B.S. Computer Science</p>
             <p style={{ color: "white" }}>Graduated: May 2021</p>
           </Container>
-          <Container size={"small"}>
+          <Container size={"small"} backgroundImage={"linear-gradient(to right, #642b73, #c6426e)"}>
+            <h3 style={{ color: "white" }}>University of Illinois Urbana-Champaign</h3>
+            <p style={{ color: "white" }}>B.S. Computer Science</p>
+            <p style={{ color: "white" }}>Graduated: May 2021</p>
+          </Container>
+          <Container size={"small"} backgroundImage={"linear-gradient(to right, #642b73, #c6426e)"}>
+            <h3 style={{ color: "white" }}>University of Illinois Urbana-Champaign</h3>
+            <p style={{ color: "white" }}>B.S. Computer Science</p>
+            <p style={{ color: "white" }}>Graduated: May 2021</p>
+          </Container>
+          <Container size={"small"} backgroundImage={"linear-gradient(to right, #642b73, #c6426e)"}>
             <h3 style={{ color: "white" }}>University of Illinois Urbana-Champaign</h3>
             <p style={{ color: "white" }}>B.S. Computer Science</p>
             <p style={{ color: "white" }}>Graduated: May 2021</p>
