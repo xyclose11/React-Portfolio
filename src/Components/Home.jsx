87f25c4cdb036539d8a89d14b28e1b5f366b1d3b
Harris G <harrisgus1@hotmail.com>
2024-07-17 19:34:55 +0000

    Altered background & title styling

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,34 +1,16 @@
-/**
- * Home component
- *
- * The section at the top of the page to display image of your
- * choice, name and title that describes your career focus.
- */
-
 import React from "react";
 import arrowSvg from "../images/down-arrow.svg";
 import PropTypes from "prop-types";
 
-/**
- * Home background image
- *
- * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
- * represents what you see in that image.
- *
- *
- * Need an image? Check out https://unsplash.com to download a photo you
- * freely use on your site.
- */
-import image from "../images/thinking_man.jpg";
+// import image from "../images/thinking_man.jpg";
 
 const imageAltText = "Thinking Man";
 
 const Home = ({ name, title }) => {
   return (
     <section id="home" className="min-height">
-      <img className="background" src={image} alt="" />
-      <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
+      {/* <img className="background" src={image} alt="" /> */}
+      <div style={{ position: "absolute", top: "4rem", left: "8rem", width: "25rem" }}>
         <h1>{name}</h1>
         <h2>{title}</h2>
       </div>
