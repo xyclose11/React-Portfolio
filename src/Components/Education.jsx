52c0dfcea46f85adab7542a6dbcbf0502517cc5f
Harris G <harrisgus1@hotmail.com>
2024-07-17 20:18:21 +0000

    Updated education styles

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -1,47 +1,58 @@
 import React from "react";
+import Container from "./Components";
 
 const Education = () => {
   return (
     <section
       className="padding"
       id="education"
-      style={{ backgroundColor: "#f9f9f9", borderRadius: "5px", padding: "20px" }}
+      style={{ backgroundColor: "#f9f9f9", borderRadius: "5px", padding: "20px", display: "flex", flexDirection: "column" }}
     >
       <h2 style={{ textAlign: "center", color: "#333", marginBottom: "20px" }}>Education</h2>
-      <div
-        className="education"
-        style={{ display: "flex", justifyContent: "center", alignItems: "center" }}
-      >
-        <div
-          style={{
-            textAlign: "center",
-            boxShadow: "0px 0px 10px rgba(0,0,0,0.1)",
-            padding: "20px",
-            borderRadius: "5px",
-          }}
-        >
-          <h3 style={{ color: "#555" }}>Valparaiso University</h3>
-          <p style={{ color: "#777" }}>B.S. Computer Science</p>
-          <p style={{ color: "#777" }}>Graduating: May 2025</p>
+    
+        <div className="education" style={{ display: "flex", justifyContent: "space-evenly" }}>
+          <Container size={"small"}>
+            <h3 style={{ color: "white" }}>University of Illinois Urbana-Champaign</h3>
+            <p style={{ color: "white" }}>B.S. Computer Science</p>
+            <p style={{ color: "white" }}>Graduated: May 2021</p>
+          </Container>
+          <Container size={"small"}>
+            <h3 style={{ color: "white" }}>University of Illinois Urbana-Champaign</h3>
+            <p style={{ color: "white" }}>B.S. Computer Science</p>
+            <p style={{ color: "white" }}>Graduated: May 2021</p>
+          </Container>
+          <Container size={"small"}>
+            <h3 style={{ color: "white" }}>University of Illinois Urbana-Champaign</h3>
+            <p style={{ color: "white" }}>B.S. Computer Science</p>
+            <p style={{ color: "white" }}>Graduated: May 2021</p>
+          </Container>
         </div>
-      </div>
-      <div
-        className="education"
-        style={{ display: "flex", justifyContent: "center", alignItems: "center" }}
-      >
-        <div
-          style={{
-            textAlign: "center",
-            boxShadow: "0px 0px 10px rgba(0,0,0,0.1)",
-            padding: "20px",
-            borderRadius: "5px",
-          }}
-        >
-          <h3 style={{ color: "#555" }}>Valparaiso University</h3>
-          <p style={{ color: "#777" }}>M.S. Cyber Security</p>
-          <p style={{ color: "#777" }}>Graduating: May 2025</p>
+
+      <div style={{marginTop: "30px"}}>
+
+        <h2 style={{ textAlign: "center", color: "#333", marginBottom: "20px" }}>External Courses</h2>
+
+        <div className="education" style={{ display: "flex", justifyContent: "space-evenly" }}>
+          <Container size={"small"}>
+            <h3 style={{ color: "white" }}>University of Illinois Urbana-Champaign</h3>
+            <p style={{ color: "white" }}>B.S. Computer Science</p>
+            <p style={{ color: "white" }}>Graduated: May 2021</p>
+          </Container>
+          <Container size={"small"}>
+            <h3 style={{ color: "white" }}>University of Illinois Urbana-Champaign</h3>
+            <p style={{ color: "white" }}>B.S. Computer Science</p>
+            <p style={{ color: "white" }}>Graduated: May 2021</p>
+          </Container>
+          <Container size={"small"}>
+            <h3 style={{ color: "white" }}>University of Illinois Urbana-Champaign</h3>
+            <p style={{ color: "white" }}>B.S. Computer Science</p>
+            <p style={{ color: "white" }}>Graduated: May 2021</p>
+          </Container>
         </div>
-      </div>
+
+        </div>
+
+
     </section>
   );
 };
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,34 +1,5 @@
-/**
- * Portfolio component
- *
- * Highlights some of  your creations. These can be designs, websites,
- * open source contributions, articles you've written and more.
- *
- * This is a great area for you to to continually add to and refine
- * as you continue to learn and create.
- */
-
 import React from "react";
 
-/**
- * Desk image
- *
- * Below is a sample desk image. Feel free to update this to an image of your choice,
- * updating below imageAltText to string that represents what you see in that image.
- *
- * Need an image? Check out https://unsplash.com to download a photo you
- * freely use on your site.
- */
-import image from "../images/raspberry_pi.jpg";
-
-const imageAltText = "Image of a rustic wooden desk with a laptop, and a raspberry pi";
-
-/**
- * Project list
- *
- * An array of objects that will be used to display for your project
- * links section. Below is a sample, update to reflect links you'd like to highlight.
- */
 const projectList = [
   {
     title: "Auction Commerce Website",
@@ -65,23 +36,13 @@ const projectList = [
 const Portfolio = () => {
   return (
     <section className="padding" id="portfolio">
-      <h2 style={{ textAlign: "center" }}>Portfolio</h2>
+      <div style={{ background: "rgba(255,255,255,0.75)", boxShadow: "20px 20px 500px rgba(0,0,0,0.1)" }}>
+        <h2 style={{ textAlign: "center", paddingBottom: "20px" }}>Projects</h2>
+      </div>
       <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
-        <div style={{ maxWidth: "40%", alignSelf: "center" }}>
-          <img
-            src={image}
-            style={{
-              height: "90%",
-              width: "100%",
-              objectFit: "cover",
-              animation: "1s ease-out 0s 1 slideInLeft",
-            }}
-            alt={imageAltText}
-          />
-        </div>
         <div className="container">
           {projectList.map((project) => (
-            <div className="box" key={project.title}>
+            <div className="box" key={project.title} style={{ backgroundColor: "white", boxShadow: "0 0 5px rgba(0,0,0,0.1)" }}>
               <a href={project.url} target="_blank" rel="noopener noreferrer">
                 <h3 style={{ flexBasis: "40px" }}>{project.title}</h3>
               </a>
