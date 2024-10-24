71bf3dfcb9cf2491a1ea145cd7453650c18aa05b
Harris G <harrisgus1@hotmail.com>
2024-10-24 02:28:41 +0000

    format fix

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -9,18 +9,18 @@ const About = () => {
             backgroundColor: "white",
             width: "30%",
             padding: "1rem",
-            margin: "1rem",
+            margin: "3rem",
             textAlign: "center",
             borderRadius: "10px",
           }}
         >
-          <h2 style={{ fontSize: "60px" }}>About Me</h2>
+          <h3 style={{ fontSize: "48px" }}>About Me</h3>
           <p>
-            I am a self driven individual who is passionate about problem solving in the computing
-            field as no two problems are alike (most of the time). I am constantly looking for new
-            and emerging technologies to experement with and learn. While I do believe reinventing
-            the wheel may not be the best use of time, I do believe that there is a lot of benefit
-            understanding how the wheel works.
+            I am a dedicated computer scientist with a passion for solving complex computational
+            problems. I thrive on the challenge of developing efficient algorithms and exploring new
+            technologies. While I recognize the value of established solutions, I believe in the
+            importance of understanding the underlying principles to innovate and improve upon
+            existing systems.
           </p>
         </div>
         <div
@@ -28,79 +28,49 @@ const About = () => {
             backgroundColor: "white",
             width: "30%",
             padding: "1rem",
-            margin: "1rem",
+            margin: "3rem",
             textAlign: "center",
             borderRadius: "10px",
           }}
         >
-          <h2 style={{ fontSize: "50px" }}>I have experience with: </h2>
-          <div style={{ display: "flex", justifyContent: "space-between" }}>
-            <div style={{ flex: 1, marginRight: "1rem" }}>
-              <ul style={{ listStyle: "none", padding: 0, margin: 0 }}>
-                <li style={{ padding: "0.5rem 0", borderBottom: "1px solid #ddd" }}>
-                  TypeScript (JavaScript)
-                </li>
-                <li style={{ padding: "0.5rem 0", borderBottom: "1px solid #ddd" }}>
-                  C# (ASP.NET Core, MVC & Blazor)
-                </li>
-                <li style={{ padding: "0.5rem 0", borderBottom: "1px solid #ddd" }}>
-                  Java (Spring Boot, Swing)
-                </li>
-
-                <li style={{ padding: "0.5rem 0", borderBottom: "1px solid #ddd" }}>
-                  MongoDB (Mongoose)
-                </li>
-                <li style={{ padding: "0.5rem 0", borderBottom: "1px solid #ddd" }}>
-                  Python (Django, Flask)
-                </li>
-                <li style={{ padding: "0.5rem 0", borderBottom: "1px solid #ddd" }}>Git</li>
+          <h3 className="title">Technical Skills</h3>
+          <div className="skills-grid">
+            <div>
+              <ul className="skills-list">
+                <li>TypeScript (JavaScript, Node.js, jQuery)</li>
+                <li>Java (Spring Boot, Swing, JavaFX)</li>
+                <li>React (Next.js, Vite, Webpack)</li>
+                <li>C# (ASP.NET Core, MVC, Blazor, Entity Framework Core)</li>
+                <li>SQL (PostgreSQL, SQL Server, MySQL)</li>
+                <li>Python (Django, Flask)</li>
+                <li>XML</li>
+                <li>JUnit</li>
+                <li>Vim</li>
+                <li>C</li>
+                <li>Git</li>
               </ul>
             </div>
-            <div style={{ flex: 1, marginLeft: "1rem" }}>
-              <ul style={{ listStyle: "none", padding: 0, margin: 0 }}>
-                <li style={{ padding: "0.5rem 0", borderBottom: "1px solid #ddd" }}>
-                  SQL (Postgresql, SQL Server)
-                </li>
-                <li style={{ padding: "0.5rem 0", borderBottom: "1px solid #ddd" }}>
-                  React (Next.js)
-                </li>
-                <li style={{ padding: "0.5rem 0", borderBottom: "1px solid #ddd" }}>
-                  Node.js (Express)
-                </li>
-                <li style={{ padding: "0.5rem 0", borderBottom: "1px solid #ddd" }}>Rust</li>
-                <li style={{ padding: "0.5rem 0", borderBottom: "1px solid #ddd" }}>Neovim</li>
-                <li style={{ padding: "0.5rem 0", borderBottom: "1px solid #ddd" }}>C</li>
-                <li style={{ padding: "0.5rem 0", borderBottom: "1px solid #ddd" }}>Go</li>
-                <li style={{ padding: "0.5rem 0", borderBottom: "1px solid #ddd" }}>Linux</li>
-                <li style={{ padding: "0.5rem 0", borderBottom: "1px solid #ddd" }}>HTML/CSS</li>
+            <div>
+              <ul className="skills-list">
+                <li>HTML</li>
+
+                <li>Docker</li>
+                <li>Redux</li>
+                <li>Jenkins</li>
+                <li>Nmap</li>
+                <li>Linux (Ubuntu, Mint, Kali)</li>
+                <li>DevExpress</li>
+                <li>Apache</li>
+                <li>C/C++</li>
+                <li>Go</li>
+                <li>MongoDB</li>
+                <li>Svelte</li>
+                <li>CSS</li>
+                <li>GraphQL</li>
+                <li>Microsoft Office</li>
               </ul>
             </div>
           </div>
-        </div>
-        <div
-          style={{
-            backgroundColor: "white",
-            width: "30%",
-            padding: "1rem",
-            margin: "1rem",
-            textAlign: "center",
-            borderRadius: "10px",
-          }}
-        >
-          <h2 style={{ fontSize: "40px", marginBottom: "1rem" }}>Interests</h2>
-          <ul style={{ listStyle: "none", padding: 0, margin: 0 }}>
-            <li style={{ padding: "0.5rem 0", borderBottom: "1px solid #ddd" }}>Rust</li>
-            <li style={{ padding: "0.5rem 0", borderBottom: "1px solid #ddd" }}>
-              Full Stack Development
-            </li>
-            <li style={{ padding: "0.5rem 0", borderBottom: "1px solid #ddd" }}>
-              Parallel Computing
-            </li>
-            <li style={{ padding: "0.5rem 0", borderBottom: "1px solid #ddd" }}>
-              Data Structures & Algorithms
-            </li>
-            <li style={{ padding: "0.5rem 0", borderBottom: "1px solid #ddd" }}>Philosophy</li>
-          </ul>
         </div>
       </div>
     </section>
