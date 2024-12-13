76ac93476c2e504ddcc6da84021c4bb4db0e83d6
Harris G <harrisgus1@hotmail.com>
2024-12-13 00:29:38 +0000

    updated descriptions

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -6,8 +6,7 @@ import MelinIMG from "../images/Melin.png";
 const projectList = [
   {
     title: "Portfolio Website",
-    description:
-      "Created a portfolio website using React, to showcase my projects and skills.",
+    description: "Created a portfolio website using React, to showcase my projects and skills.",
     url: "https://github.com/xyclose11/React-Portfolio", // Update with your project URL or GitHub project page or make a new react page that displays it
   },
   {
@@ -33,7 +32,7 @@ const projectList = [
     description:
       "Following the guide from the book 'Create an interpreter using Go' to learn more about interpreters and eventually create my own.",
     url: "https://github.com/xyclose11/Interpreter",
-  }
+  },
 ];
 
 const Portfolio = () => {
@@ -48,7 +47,24 @@ const Portfolio = () => {
       <ProjectShowcase
         title="Melin"
         description="An academic reference management applcaiton that allows users to create, edit, and delete references. Implemented PDF, and JSON parsing, alongside a custom barcode implementation that allows users to automatically scan a barcode and upload the reference data, using the mobile application. Also implemented sorting, filtering, tagging, grouping, user auth and searching functionality."
-        technologies={["ASP.NET Core", "C#", "Entity Framework Core", "PostgreSQL", "React.js", "TypeScript", "Jenkins", "HTML", "CSS", "Shadcn", "Docker", "JWT", "Swagger", "XUnit", "React Native", "Nginx"]}
+        technologies={[
+          "ASP.NET Core",
+          "C#",
+          "Entity Framework Core",
+          "PostgreSQL",
+          "React.js",
+          "TypeScript",
+          "Jenkins",
+          "HTML",
+          "CSS",
+          "Shadcn",
+          "Docker",
+          "JWT",
+          "Swagger",
+          "XUnit",
+          "React Native",
+          "Nginx",
+        ]}
         ghLink={"https://github.com/xyclose11/Melin"}
       >
         <div>
@@ -61,60 +77,68 @@ const Portfolio = () => {
 
       <ProjectShowcase
         title="Pain(t)"
-        description="My take on Microsoft paint. Which allows users to draw, erase, and save their drawings, alongside much more. This project was completed in 7 weeks for CS250: Object Oriented Programming at Valparaiso University."
+        description="Reimplementation of Microsoft paint. Users are able to draw, erase, and save their drawings, along with a multitude of image manipulation tools such as 'select', 'crop', and more. This project was completed in 7 weeks for CS250: Object Oriented Programming at Valparaiso University."
         technologies={["Java", "JavaFX", "UML", "Maven", "JUnit"]}
         ghLink={"https://github.com/xyclose11/Paint"}
       >
         <div>
-        <figure className="relative max-w-full transition-all duration-300 cursor-pointer filter">
+          <figure className="relative max-w-full transition-all duration-300 cursor-pointer filter">
             <img src={PaintPNG} alt="Melin" className="h-auto w-full shadow-xl rounded-t-3xl" />
             <figcaption className="absolute px-4 text-lg text-white bottom-6">Paint</figcaption>
-        </figure>
+          </figure>
         </div>
       </ProjectShowcase>
 
       <ProjectShowcase
         title="Citation Generator"
-        description="This was a project for a Full Stack Development course at Valparaiso University. The result of this project is a website that can handle citations in multiple formats.
+        description="A Full Stack application that will store and organize a users academic references. Can export into CSV, BibTex, CSL-JSON and over 10,000 citation styles.
         This project was completed in 15 weeks in a group of 4 students. I was responsible for the citation generation process, user authentication, the parsing of the citation data, BibTex error handling, and more.
         "
         technologies={["React", "Node.js", "Next.js", "MongoDB", "HTML", "CSS", "TypeScript"]}
         ghLink={"https://github.com/xyclose11/cashmoneycitations"}
-      >
-      </ProjectShowcase>
-      
+      ></ProjectShowcase>
+
       <ProjectShowcase
         title="Note-ify"
         description="A note taking app that allows users to create, edit, and delete notes. Also implemented
         sorting, filtering, tagging, grouping, user auth and searching functionality."
-        technologies={["ASP.NET Core", "C#", "Entity Framework Core", "PostgreSQL", "HTML", "CSS", "jQuery", "TypeScript"]}
+        technologies={[
+          "ASP.NET Core",
+          "C#",
+          "Entity Framework Core",
+          "PostgreSQL",
+          "HTML",
+          "CSS",
+          "jQuery",
+          "TypeScript",
+        ]}
         ghLink={"https://github.com/xyclose11/Notify"}
       />
 
-
-
-      <ProjectShowcase 
-      title="Auction Commerce Website"
-      technologies={["Django", "HTML", "CSS", "JavaScript"]}
-      description="Created a website for a mock auction company using Django, that inlcudes a login page, product listings, and more."
-      ghLink={"https://github.com/xyclose11/cautious-eureka"}
+      <ProjectShowcase
+        title="Auction Commerce Website"
+        technologies={["Django", "HTML", "CSS", "JavaScript"]}
+        description="Created a website for a mock auction company using Django, that inlcudes a login page, product listings, and more."
+        ghLink={"https://github.com/xyclose11/cautious-eureka"}
       />
 
       <ProjectShowcase
-      title="Garden X"
-      technologies={["Flask", "SQL", "Python", "Javascript", "HTML/CSS"]}
-      description="Designed and implemented a web application that allows users to lookup a plant and get information about it. Uses Flash, SQL, Python, Javascript, and HTML/CSS. Submitted to CS50 as a final project."
-      ghLink={"https://www.youtube.com/watch?v=MAhIyux1Ia0"}
-      >
-      </ProjectShowcase>
+        title="Garden X"
+        technologies={["Flask", "SQL", "Python", "Javascript", "HTML/CSS"]}
+        description="Designed and implemented a web application that allows users to lookup a plant and get information about it. Uses Flash, SQL, Python, Javascript, and HTML/CSS. Submitted to CS50 as a final project."
+        ghLink={"https://www.youtube.com/watch?v=MAhIyux1Ia0"}
+      ></ProjectShowcase>
 
       <h2 style={{ textAlign: "center", paddingBottom: "20px" }}>Smaller Projects/In Progress</h2>
-
 
       <div style={{ display: "flex", flexDirection: "row", paddingTop: "0rem" }}>
         <div className="container">
           {projectList.map((project) => (
-            <div className="box" key={project.title} style={{ backgroundColor: "#f5f5f5", boxShadow: "0 0 5px rgba(0,0,0,0.1)" }}>
+            <div
+              className="box"
+              key={project.title}
+              style={{ backgroundColor: "#f5f5f5", boxShadow: "0 0 5px rgba(0,0,0,0.1)" }}
+            >
               <a href={project.url} target="_blank" rel="noopener noreferrer">
                 <h3 style={{ flexBasis: "40px" }}>{project.title}</h3>
               </a>
